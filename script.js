d5d67437d243b5257ec2b804bc4769fbeef5c585
Felipe00lk <felipecruzfc185@gmail.com>
2025-12-11 11:04:32 -0300

    primeiro

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function scrollToSection(id) {
   const section = document.getElementById(id);
   section.scrollIntoView({ behavior: "smooth" });
 }
+const dark=document.querySelector("#dark");
+dark.onclick= () =>{
+  document.body.classList.toggle("escuro");
+};
